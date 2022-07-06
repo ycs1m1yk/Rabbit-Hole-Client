049dbca9688a714faad932435bf99ca788312d26
Juhyeon <gomi.dev1755@gmail.com>
2022-07-06 21:34:21 +0900

    init: React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Board from './pages/board';
+import Home from './pages/home';
+import Mentoring from './pages/mentoring';
+import Mypage from './pages/mypage';
+import Projects from './pages/projects';
 
 function App() {
   return (
-    <div>fdsfdf</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/mentoring" element={<Mentoring />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/mentoring.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Mentoring() {
+  return (
+    <div>Mentoring</div>
+  );
+}
