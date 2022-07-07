37f1cd9c71a5e4e7fa7a3cc796ea949ee082aa9e
Juhyeon <gomi.dev1755@gmail.com>
2022-07-07 14:18:56 +0900

    style: font, palette

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -1,9 +1,30 @@
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
+import EliceBold from './fonts/EliceDigitalBaeumOTF_Bold.woff';
+import EliceRegular from './fonts/EliceDigitalBaeumOTF_Regular.woff';
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
+  @font-face {
+    font-family: 'EliceBold';
+    src: url(${EliceBold}) format('woff');
+  }
+  @font-face {
+    font-family: 'EliceRegular';
+    src: url(${EliceRegular}) format('woff');
+  }
+  html {
+    font-size: 62.5%;
+  }
   html, body{
+    font-family: "EliceRegular", sans-serif;
+  };
+  a {
+    text-decoration: none;
+    color: inherit;
+  };
+  * {
+    box-sizing: border-box;
   };
 `;
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,27 @@
 import { DefaultTheme } from 'styled-components';
 
-const theme: DefaultTheme = {
+const size = {
+  mobile: '767px',
+  desktop: '768px',
+};
 
+const theme: DefaultTheme = {
+  palette: {
+    eliceViolet: '#6700E6',
+    lightViolet: '#6F52D9',
+    eliceBlue: '#524FA1',
+    borderGray: '#DEE2E6',
+    gray: '#747474',
+    black: '#151618',
+  },
+  status: {
+    verifyGreen: '#59FE7D',
+    warningRed: '#FF0000',
+  },
+  devices: {
+    desktop: `min-width: ${size.desktop}`,
+    mobile: `max-width: ${size.mobile}`,
+  },
 };
 
 export default theme;
