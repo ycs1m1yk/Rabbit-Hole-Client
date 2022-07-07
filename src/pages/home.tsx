57f5b08a27dfb06d65bba71338ca389984305819
Jaehyeok <milkboy2564@naver.com>
2022-07-07 21:38:29 +0900

    test: Select Option에 따른 API 호출 테스트

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 import SelectBox from '../components/selectBox';
 
 export default function Home() {
-  const selectItem: string[] = ['Own Data', 'Home', 'Select', 'My Page', 'My Page', 'My Page'];
+  const selectItem: string[] = ['1', '2', '3', '4', '5', '6'];
   const [selectedOption, SetSelectedOption] = useState('');
 
-  const handleSelectedOptionClick = (option) => console.log(option);
+  // API 요청 테스트
+  const BASE_URL = 'https://yts.mx/api/v2/list_movies.json?page_number=';
+  const handleSelectedOptionClick = async (pageNum) => {
+    const response = await fetch(`${BASE_URL}${pageNum}`);
+    const { data: { movies } } = await response.json();
+
+    console.log(`Request API URL : ${BASE_URL}${pageNum}`, movies);
+  };
+
   return (
     <>
       <div>Home</div>
