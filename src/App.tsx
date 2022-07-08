10f2e3fcc929f8b05a3aeccc176a4492da65c242
Jaehyeok <milkboy2564@naver.com>
2022-07-09 00:20:36 +0900

    fix: alias 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Footer from './components/footer';
 
 import Board from '@pages/board';
 import Home from '@pages/home';
 import Mentoring from '@pages/mentoring';
 import Mypage from '@pages/mypage';
 import Projects from '@pages/projects';
+import Footer from '@components/footer';
 
 function App() {
   return (
