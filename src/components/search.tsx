271e036c4181d14f45bad6583640b3e8e4d2e5b7
ycs1m1yk <satoly4@gmail.com>
2022-07-08 15:45:13 +0900

    Feat: search 컴포넌트 더미 작성

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,1 +1,24 @@
-// search
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSearch = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 2rem;
+
+    width: 30rem;
+    height: 4.5rem;
+
+    border: 1px solid ${(props) => props.theme.palette.borderGray};
+    border-radius: 40px;
+`;
+
+export default function Search() {
+  return (
+    <StyledSearch>
+      검색바
+    </StyledSearch>
+  );
+}
