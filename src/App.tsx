a2131dc04937000fb4262a3225a4ed7ab799a4fe
ycs1m1yk <satoly4@gmail.com>
2022-07-08 15:46:12 +0900

    Feat: App 컴포넌트에 header 사용

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Home from '@pages/home';
 import Mentoring from '@pages/mentoring';
 import Mypage from '@pages/mypage';
 import Projects from '@pages/projects';
+import Header from '@components/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/board" element={<Board />} />
