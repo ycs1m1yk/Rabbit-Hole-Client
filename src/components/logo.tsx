784f18332c7e3b24642f76d3900cc96c5885f727
ycs1m1yk <satoly4@gmail.com>
2022-07-08 14:48:38 +0900

    Feat: logo component 완성

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,1 +1,26 @@
-// logo
+import React from 'react';
+import styled from 'styled-components';
+import LogoImage from '@assets/images/rabbit-hole-logo.png';
+
+const StyledLogo = styled.img<{width: number, heigth: number}>`
+  width: ${(props) => `${props.width / 10}rem`};
+  height: ${(props) => `${props.heigth / 10}rem`};
+`;
+
+const defaultProps = {
+  src: LogoImage,
+};
+
+type LogoProps = {
+  width: number;
+  heigth: number;
+  src?: string;
+} & typeof defaultProps;
+
+export default function Logo({ width, heigth, src }: LogoProps) {
+  return (
+    <StyledLogo width={width} heigth={heigth} src={src} />
+  );
+}
+
+Logo.defaultProps = defaultProps;
