584313a3ad78d57a071d97581307db247213f6c0
Juhyeon <gomi.dev1755@gmail.com>
2022-07-09 09:05:40 +0900

    feat: markdown viewer/editor

--- src/components/markdownEditor.tsx
+++ src/components/markdownEditor.tsx
@@ -1,1 +1,23 @@
 // markdownEditor
+import React, { forwardRef } from 'react';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import Prism from 'prismjs';
+
+const MarkdownEditor = forwardRef<Editor>((props, ref) => (
+  <Editor
+    ref={ref}
+    plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
+    hooks={{
+      addImageBlobHook(blob, callback) {
+        console.log(blob);
+        callback(import.meta.env.VITE_IMAGE_SERVER_URL, '티모');
+      },
+    }}
+  />
+));
+
+export default MarkdownEditor;
--- src/components/markdownViewer.tsx
+++ src/components/markdownViewer.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+interface ViewerProps{
+  text: string,
+}
+
 // markdownViewer
+export default function MarkdownViewer({ text = '' }:ViewerProps) {
+  return (
+    <Viewer initialValue="![티모](https://c.tenor.com/sK02BXIShR0AAAAC/%ED%8B%B0%EB%AA%A8-%EB%A9%94%EB%A1%B1.gif)" />
+  );
+}
