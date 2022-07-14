8421cc4a6160cc4ccf06097fcb4a35b057bab63f
ycs1m1yk <satoly4@gmail.com>
2022-07-15 03:31:21 +0900

    Feat: 게시글 작성 버튼 조건부 랜더링

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -14,6 +14,7 @@ import { useSetRecoilState } from 'recoil';
 
 import { IArticleGetProps, IArticleProps } from '@/interfaces/interface';
 import { getAllArticle } from '@/lib/articleApi';
+import useToken from '@/hooks/useToken';
 
 const BoardContainer = styled.div`
   display: flex;
@@ -33,6 +34,10 @@ const BoardWrapper = styled.div`
     position: absolute;
     top: 8.2rem;
     right: 11rem;
+
+    &[data-user-loged-in="false"] {
+      right: 1rem;    
+    }  
   }
   & .button-posting {
     align-self: flex-end;
@@ -54,17 +59,19 @@ const SelectBoxWrapper = styled.div`
  * - [x] Modal 연결
  * - [x] articleForm 작성
  * - [x] perPage selectBox 추가
- * - [] getAllAtricles로 게시글 뿌려주기
+ * - [x] getAllAtricles로 게시글 뿌려주기
  * - [] 최신순 추천순 정렬
  * - [] 자신이 좋아요한 게시글 빨강하트
  * - [] 페이지네이션 이동
  * - [] 페이지네이션 캐싱: react-query
+ * - [] 로그인 상태 조건부 랜더링
  */
 export default function Board() {
   const [articles, setArticles] = useState<IArticleProps[]>([]);
   const [page, setPage] = useState<number>(0);
   const [perPage, setPerPage] = useState<number>(10);
   const setModalState = useSetRecoilState(modalAtom);
+  const { authInfo } = useToken();
 
   const handleModalOpen = useCallback(() => {
     setModalState('Posting');
@@ -92,10 +99,11 @@ export default function Board() {
     <SideBar type="board" />
     <BoardWrapper>
       <Search width={800} />
-      <SelectBoxWrapper className="selectbox-perpage">
+      <SelectBoxWrapper className="selectbox-perpage" data-user-loged-in={!!authInfo}>
         <SelectBox options={[5, 10, 15, 20]} defaultValue="페이지당 개수" selectedOption={perPage} setSelectedOption={setPerPage} width={70} type="register" />
       </SelectBoxWrapper>
-      <Button className="button-posting" size="medium" onClick={handleModalOpen}>게시글 등록</Button>
+      {authInfo
+      && <Button className="button-posting" size="medium" onClick={handleModalOpen}>게시글 등록</Button>}
       <PostList posts={articles.slice(0, perPage)} />
       <Pagination
         length={Math.ceil(articles.length / perPage)}
