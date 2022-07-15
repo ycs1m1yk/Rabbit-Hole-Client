c5c5bae65a8ea2732d97404aeac21ddb86705d95
ycs1m1yk <satoly4@gmail.com>
2022-07-15 19:35:00 +0900

    Feat: 로그인상태 조건부랜더링, 페이지네이션

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useQuery } from 'react-query';
 
@@ -8,6 +8,7 @@ import SelectBox from '@components/selectBox';
 import Button from '@components/button';
 import PostList from '@components/postList';
 import Pagination from '@components/pagination';
+import Loading from '@components/loading';
 
 import modalAtom from '@/recoil/modal/modalAtom';
 import { useSetRecoilState } from 'recoil';
@@ -15,6 +16,7 @@ import { useSetRecoilState } from 'recoil';
 import { IArticleGetProps, IArticleProps } from '@/interfaces/interface';
 import { getAllArticle } from '@/lib/articleApi';
 import useToken from '@/hooks/useToken';
+import { useSearchParams } from 'react-router-dom';
 
 const BoardContainer = styled.div`
   display: flex;
@@ -62,54 +64,94 @@ const SelectBoxWrapper = styled.div`
  * - [x] getAllAtricles로 게시글 뿌려주기
  * - [] 최신순 추천순 정렬
  * - [] 자신이 좋아요한 게시글 빨강하트
- * - [] 페이지네이션 이동
+ * - [x] 페이지네이션 이동
  * - [] 페이지네이션 캐싱: react-query
- * - [] 로그인 상태 조건부 랜더링
+ * - [x] 로그인 상태 조건부 랜더링
  */
 export default function Board() {
-  const [articles, setArticles] = useState<IArticleProps[]>([]);
-  const [page, setPage] = useState<number>(0);
-  const [perPage, setPerPage] = useState<number>(10);
   const setModalState = useSetRecoilState(modalAtom);
   const { authInfo } = useToken();
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [perPage, setPerPage] = useState<number>(10);
+  const [query, setQuery] = useState<IArticleGetProps>({ articleType: 'question' });
+  const [articles, setArticles] = useState<IArticleProps[]>([]);
 
   const handleModalOpen = useCallback(() => {
     setModalState('Posting');
   }, []);
 
-  const params: IArticleGetProps = {
-    articleType: 'question',
-    filter: 'date',
-    page: 1,
-    perPage,
-  };
+  const handlePage = useCallback((idx: number) => {
+    searchParams.set('page', `${idx + 1}`);
+    setSearchParams(searchParams);
+  }, []);
 
-  const { isLoading } = useQuery<any>(
-    ['articleList', 'question'],
-    () => getAllArticle(params),
+  const handlePerPage = useCallback((perPageValue: number) => {
+    searchParams.set('perPage', `${perPageValue}`);
+    searchParams.set('page', '1');
+    setSearchParams(searchParams);
+  }, []);
+
+  const {
+    isLoading, isError, data, error,
+  } = useQuery<any, Error>(
+    ['articleList', query],
+    () => getAllArticle(query),
     {
-      onSuccess: (res) => {
-        setArticles(res.articleList);
+      onSuccess: (resp) => {
+        console.log(resp);
+        setArticles(resp.articleList);
       },
     },
   );
 
-  return !isLoading && (
-  <BoardContainer>
-    <SideBar type="board" />
-    <BoardWrapper>
-      <Search width={800} />
-      <SelectBoxWrapper className="selectbox-perpage" data-user-loged-in={!!authInfo}>
-        <SelectBox options={[5, 10, 15, 20]} defaultValue="페이지당 개수" selectedOption={perPage} setSelectedOption={setPerPage} width={70} type="register" />
-      </SelectBoxWrapper>
-      {authInfo
+  useEffect(() => {
+    if (isReady) {
+      searchParams.forEach((v, k) => setQuery((q) => ({ ...q, [k]: v })));
+    } else {
+      searchParams.set('articleType', 'question');
+      searchParams.set('page', '1');
+      searchParams.set('perPage', '10');
+      setSearchParams(searchParams);
+      setIsReady(true);
+    }
+  }, [searchParams]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return (
+      <span>{`Error: ${error.message}`}</span>
+    );
+  }
+
+  return isReady && (
+    <BoardContainer>
+      <SideBar type="board" />
+      <BoardWrapper>
+        <Search width={800} />
+        <SelectBoxWrapper className="selectbox-perpage" data-user-loged-in={!!authInfo}>
+          <SelectBox
+            options={[1, 10, 15, 20]}
+            defaultValue="페이지당 개수"
+            selectedOption={perPage}
+            setSelectedOption={setPerPage}
+            requestFunc={handlePerPage}
+            width={70}
+            type="register"
+          />
+        </SelectBoxWrapper>
+        {authInfo
       && <Button className="button-posting" size="medium" onClick={handleModalOpen}>게시글 등록</Button>}
-      <PostList posts={articles.slice(0, perPage)} />
-      <Pagination
-        length={Math.ceil(articles.length / perPage)}
-        handler={setPage}
-      />
-    </BoardWrapper>
-  </BoardContainer>
+        <PostList posts={articles} />
+        <Pagination
+          length={data.totalPage}
+          start={query.page ? +(query.page) - 1 : 0}
+          handler={(pageIdx) => handlePage(pageIdx)}
+        />
+      </BoardWrapper>
+    </BoardContainer>
   );
 }
