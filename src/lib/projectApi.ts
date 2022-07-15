6fdfc98941a963f06c2452d252ca351c67b4996c
Jaehyeok <milkboy2564@naver.com>
2022-07-15 23:06:44 +0900

    feat: project detail component #1

--- src/lib/projectApi.ts
+++ src/lib/projectApi.ts
@@ -14,6 +14,14 @@ export const getAllProjects = (params: interfaces.IProjectGetParamsProps) => {
     .then((res) => res.json());
 };
 
+export const getProjectById = (projectId: string, params: interfaces.IProjectGetParamsProps) => {
+  const param = new URLSearchParams(params);
+  const par = param.toString();
+
+  return fetch(`${BASE_URL}/projects/${projectId}?${par}`)
+    .then((res) => res.json());
+};
+
 // 프로젝트 작성
 export const postProject = (token: string, bodyData: FormData) => fetch(`${BASE_URL}/projects`, {
   method: 'POST',
--- src/pages/projects/projectDetail.tsx
+++ src/pages/projects/projectDetail.tsx
@@ -1,7 +1,97 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import MarkdownViewer from '@/components/markdownViewer';
+import { getProjectById } from '@/lib/projectApi';
+
+const ProjectDetailContainer = styled.div`
+  padding: 3rem;
+`;
+
+const ProjectDetailHeader = styled.h1`
+  font-size: 2.5rem;
+`;
+
+const ProjectContentContainer = styled.div`
+  width: 100%;
+  height: 50rem;
+  margin: 2rem 0;
+  display: grid;
+  grid-template-columns: 30% 70%;
+`;
+
+const ProjectImage = styled.img`
+  width: 80%;
+`;
+
+const ProjectInfo = styled.div`
+  overflow-y: auto;
+`;
+
+const ProjectInfoTitle = styled.h2`
+  margin: 2rem 0;
+`;
+
+const ProjectTitle = styled.div`
+  font-size: 1.5rem;
+`;
+
+const ProjectAuthor = styled.div`
+  font-size: 1.5rem;
+`;
+
+const ProjectTag = styled.span``;
+
+const ProjectDescription = styled.div`
+  font-size: 1.5rem;
+`;
+
+const ReplyContainer = styled.div`
+  background-color: green;
+`;
 
 function ProjectDetail() {
-  return <div>ProjectDetail</div>;
+  const [searchParams] = useSearchParams();
+  const projectId = searchParams.get('projectId');
+  const params = { page: 1, perPage: 5 };
+  let project;
+
+  if (projectId) {
+    const { data } = useQuery<any>(['projectDetail', projectId], () => getProjectById(projectId, params), {
+      staleTime: 180000,
+    });
+    if (data) {
+      project = data.projectInfo;
+    }
+  }
+  console.log(project);
+
+  return project && (
+    <ProjectDetailContainer>
+      <ProjectDetailHeader>프로젝트 상세</ProjectDetailHeader>
+      <ProjectContentContainer>
+        {project && <ProjectImage src={project.thumbnail} />}
+        <ProjectInfo>
+          <ProjectInfoTitle>제목</ProjectInfoTitle>
+          <ProjectTitle>{project.title}</ProjectTitle>
+          <ProjectInfoTitle>작성자</ProjectInfoTitle>
+          <ProjectAuthor>{project.author}</ProjectAuthor>
+          {project.tags.length > 0 ? <ProjectInfoTitle>태그</ProjectInfoTitle> : null}
+          {project.tags.length > 0
+            ? project.tags.map((tag: string) => <ProjectTag>{tag}</ProjectTag>) : null}
+          <ProjectInfoTitle>프로젝트 한 줄 소개</ProjectInfoTitle>
+          <ProjectAuthor>{project.shortDescription}</ProjectAuthor>
+          <ProjectInfoTitle>프로젝트 상세</ProjectInfoTitle>
+          <ProjectDescription>
+            <MarkdownViewer text={project.description} />
+          </ProjectDescription>
+        </ProjectInfo>
+      </ProjectContentContainer>
+      <ReplyContainer>답글</ReplyContainer>
+    </ProjectDetailContainer>
+  );
 }
 
 export default ProjectDetail;
