525f3a0ecd64864e59fa8d90d3d591a50aa6b587
ycs1m1yk <satoly4@gmail.com>
2022-07-17 00:45:10 +0900

    Fix: authAtom이 AuthTypes 사용

--- src/recoil/auth/authAtom.ts
+++ src/recoil/auth/authAtom.ts
@@ -1,20 +1,13 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
-
-interface AuthProps {
-  userName: string,
-  token: string,
-  expire: string,
-  userId: string,
-  carrots: number,
-}
+import { AuthTypes } from '@interfaces/type';
 
 const { persistAtom } = recoilPersist({
   key: 'RabbitHoleAuth',
   storage: localStorage,
 });
 
-const authAtom = atom<AuthProps | null>({
+const authAtom = atom<AuthTypes>({
   key: 'authState', // 해당 atom의 고유 key
   default: null, // 기본값
   effects_UNSTABLE: [persistAtom],
